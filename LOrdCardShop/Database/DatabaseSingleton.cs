acc4f3b844cf42e3fc9451c179e768d86b2a3cd5
Christopher Hardy Gunawan <christopher.hygunawan@gmail.com>
2025-03-12 21:49:50 +0700

    add database, model, db connection

---
+++ LOrdCardShop/Database/DatabaseSingleton.cs
@@ -0,0 +1,20 @@
+﻿using LOrdCardShop.Model;
+
+namespace LOrdCardShop.Database
+{
+    public class DatabaseSingleton
+    {
+        public static LordCardShopDatabaseEntities db = null;
+
+        private DatabaseSingleton() { }
+
+        public static LordCardShopDatabaseEntities GetInstance()
+        {
+            if (db == null)
+            {
+                db = new LordCardShopDatabaseEntities();
+            }
+            return db;
+        }
+    }
+}
---
+++ LOrdCardShop/Repositories/UsersRepository.cs
@@ -0,0 +1,24 @@
+﻿using LOrdCardShop.Database;
+using LOrdCardShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LOrdCardShop.Repositories
+{
+    public class UsersRepository
+    {
+        private static LordCardShopDatabaseEntities db = DatabaseSingleton.GetInstance();
+
+        public static List<User> GetAllUsers()
+        {
+            return db.Users.ToList();
+        }
+
+        public static User GetUserByID(int id)
+        {
+            return db.Users.Find(id);
+        }
+    }
+}
